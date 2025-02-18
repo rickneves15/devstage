e2f15593a98553901daff4c81dd25a59c859c50e
Richard Neves <ricknevesbc@icloud.com>
2025-02-18 14:11:56 -0300

    feat: implements home and invite pages
    Adds new pages for home and invitation, improving user experience and providing key information about the event and invitation process.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable} ${oxanium.variable}`}>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top mg:bg-right-top">
-        {children}
+        <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+          {children}
+        </main>
       </body>
     </html>
   )
