b3152f203eebbe718af396be2c1482d77d4dedbc
Richard Neves <ricknevesbc@icloud.com>
2025-02-18 11:30:35 -0300

    Style(layout): Improves page styling
    Adds background image and updates font styles for improved visual appeal.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,35 @@
-import "./globals.css";
+import './globals.css'
 
-import type { Metadata } from "next";
-import { Montserrat, Oxanium } from "next/font/google";
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "Dev Stage",
-  description: "Dev Stage",
-};
+  title: 'Dev Stage',
+  description: 'Dev Stage',
+}
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "600"],
-  variable: "--font-montserrat",
-});
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-montserrat',
+})
 
 const oxanium = Oxanium({
-  subsets: ["latin"],
-  weight: ["500", "600"],
-  variable: "--font-oxanium",
-});
+  subsets: ['latin'],
+  weight: ['500', '600'],
+  variable: '--font-oxanium',
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={`${montserrat.variable} ${oxanium.variable}`}>
-      <body>{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top mg:bg-right-top">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
